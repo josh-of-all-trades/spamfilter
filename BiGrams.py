5050c7b1ca2738dee51f31bbe84430d6b4902bb1
Josh Rojas <rojos@seas.upenn.edu>
2013-12-16 22:53:36 -0500

    Shadia's edits from my computer

--- BiGrams.py
+++ BiGrams.py
@@ -2,22 +2,28 @@ from collections import defaultdict
 
 def BiGramsTokenizer (string) :
 	str = []
-	for word in string[1].split():
+	for word in string:
+		#print "word is ", word
 		if word == 'JSNUM' or word == 'JSNUM3' or word == 'JSNUM6' or word == 'JSMONEY':
 			str.append(word)
-			if word.isalpha():
-				str.append(word.tolower())
-	print str
+		if word.isalpha():
+			str.append(word.lower())
+	#print "printing str ", str
 	return str
 
 def createVocab (allStrings) :
 	dir = {}
 	for string in allStrings:
+		#print "string ", string
 		string = BiGramsTokenizer(string)
 		for i in range(len(string)-1):
 			word1 = string[i]
 			word2 = string[i+1]
-			dir[(word1,word2)] += 1
+			#print word1, word2
+			if (word1, word2) in dir:
+				dir[(word1,word2)] += 1
+			else :
+				dir[(word1,word2)] = 0
 	return dir
 
 def biNaiveBayes (spamham) :
@@ -33,7 +39,7 @@ def biNaiveBayes (spamham) :
 		total = len(countdict.keys())
 		for ele in countdict:
 			countdict[ele] = float(countdict[ele] + m) / float(len(allStrings) + total/m)
-			print countdict[ele]
+			#print "printing countdict ", countdict[ele]
 		classes.append((i,countdict))
 		i += 1
 	return classes
