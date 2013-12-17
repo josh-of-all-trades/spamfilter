b11ec731f5271b6973c80bffde58e01b4ab68d75
Josh Rojas <rojos@seas.upenn.edu>
2013-12-16 22:02:59 -0500

    pushing to pull

--- tokenizer.py
+++ tokenizer.py
@@ -65,8 +65,46 @@ def tokenizedirs(dirs):
 	
 	return classes
 	
+def BiGramsTokenizer (string) :
+	str = []
+	for word in string.split():
+		if word == 'JSNUM' or word == 'JSNUM3' or word == 'JSNUM6' or word == 'JSMONEY':
+			str.append(word)
+			if word.isalpha():
+				str.append(word.tolower())
+	return str
+
+def createVocab (allStrings) :
+	dir = {}
+	for string in allStrings:
+		string = BiGramTokenizer(string)
+		for i in range[len(string)-1]:
+			word1 = string[i]
+			word2 = string[i+1]
+			dir[(word1,word2)] += 1
+	return dir
+
+def biNaiveBayes (spamham) :
+	vocab = defaultdict(int)
+	for allStrings in spamham:
+		vocab.update(createVocab(allStrings))
+	classes = []
+	i = 0
+	for allStrings in spamham:
+		countdict = defaultdict(int, vocab)
+		countdict.update(createVocab(allStrings))
+		m = 1
+        total = len(countdict.keys())
+        for ele in countdict:
+        	countdict[ele] = float(countdict[ele] + m) / float(len(allStrings) + total/m)  
+        classes.append((i,countdict))
+        i += 1
+    return classes
+		
+	
 if __name__ == '__main__':
     print 'argv', sys.argv
     print "Usage:", sys.argv[0], "classdir1 classdir2 [classdir3...] testfile"
     dirs = sys.argv[1:]
-    tokenizedirs(dirs)
+    tk = tokenizedirs(dirs)
+    biNaiveBays(tk[1])
