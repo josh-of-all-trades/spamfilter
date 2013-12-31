aacd1c85ff580f06766d8134cd36089847bec96d
Josh Rojas <rojos@seas.upenn.edu>
2013-12-31 14:08:26 -0800

    Finished version i hope

--- JoshuaRojas_ShadiaAl-Shafei_CIS391Homework6/Predictor.py
+++ JoshuaRojas_ShadiaAl-Shafei_CIS391Homework6/Predictor.py
@@ -19,7 +19,10 @@ class Predictor:
         self.__spamFrequency = 0
 
         # do training on spam and ham
-        self.__trained = self.__train__()
+        self.__trained = []
+        self.__naivedic = {}
+        
+        [self.__trained, self.__naivedic] = self.__train__()
 
     def __train__(self):
         '''train model on spam and ham'''
@@ -29,7 +32,8 @@ class Predictor:
         hamCount = len(glob.glob(self.__hamFolder+'/*'))
         #self.__spamFrequency = 1.0*spamCount/(spamCount+hamCount)
         toks = tokenizedirs([self.__spamFolder, self.__hamFolder])
-        return biNaiveBayes(toks[:][1])
+        nb = naivebayes([self.__spamFolder, self.__hamFolder])
+        return [biNaiveBayes(toks[:][1]), nb]
         
     def getTrained(self):
     	return self.__trained
@@ -40,6 +44,9 @@ class Predictor:
         True - filename is spam
         False - filename is not spam (is ham)
         '''
+        
+        unians = classify(self.__naivedic, filename)
+        
         # do prediction on filename
         answers = []
         bigrams = bigramify(filename)
@@ -56,13 +63,31 @@ class Predictor:
         		score = score + math.log(tempscore)
         	answers.append((score, i))
         
+        
+        
+        
         answers.sort()
         answers.reverse()
         
-        if (answers[0][1] == 0):
-        	return False
-        else :
+        realanswers = []
+        
+        for val in unians:
+        	if val[1] == self.__spamFolder:
+        		for ans in answers:
+        			if ans[1] == 0:
+        				realanswers.append(((ans[0]+val[0]*2), 0))
+        	else:
+        		for ans in answers:
+        			if ans[1] == 1:
+        				realanswers.append(((ans[0]+val[0]*2), 1))
+        				
+        realanswers.sort()
+        realanswers.reverse()
+        
+        if (realanswers[0][1] == 0):
         	return True
+        else :
+        	return False
 
 
 def bigramify(filename):
@@ -161,7 +186,82 @@ def biNaiveBayes (spamham) :
 		classes.append((i,countdict))
 		i += 1
 	return classes
-		
+	
+def naivebayes (dirs):
+    """Train and return a naive Bayes classifier.  
+    The datastructure returned is an array of tuples, one tuple per
+    class; each tuple contains the class name (same as dir name)
+    and the multinomial distribution over words associated with
+    the class"""
+    # Set up the vocabulary for all files in the training set
+    vocab = defaultdict(int)
+
+    for dir in dirs:    
+    	
+        vocab.update(files2countdict(glob.glob(dir+"/*")))
+    # Set all counts to 0
+    vocab = defaultdict(int, zip(vocab.iterkeys(), [0 for i in vocab.values()]))
+    
+    #this is the tokenizer that I was working on implementing however
+    #I noticed that when I removed it I had a better percentage correct so I 
+    #decided to leave it out
+    
+    classes = []
+    for dir in dirs:
+        print dir
+        # Initialize to zero counts   
+        countdict = defaultdict(int, vocab)
+        # Add in counts from this class
+        countdict.update(files2countdict(glob.glob(dir+"/*")))
+
+       
+        #***
+        # Here turn the "countdict" dictionary of word counts into
+        # into a dictionary of smoothed word probabilities
+        #***
+        
+        for key in countdict:
+        	countdict[key] = (countdict[key] + 1.0) / (len(dirs) + len(vocab))
+        
+        classes.append((dir,countdict))
+    return classes
+
+def classify (classes, filename):
+    """Given a trained naive Bayes classifier returned by naivebayes(), and
+    the filename of a test document, d, return an array of tuples, each
+    containing a class label; the array is sorted by log-probability 
+    of the class, log p(c|d)"""
+    answers = []
+    #print 'Classifying', filename
+    for c in classes:
+        score = 0
+        #***
+        # Here, compute the naive bayes score for a file for a given class by:
+        # 1. Reading in each word, and converting it to lower case (see code below)
+        f = open(filename, 'r')
+        str = f.read().lower()
+        tok = str.split()
+        # 2. Adding  the log probability of that word for that class
+        #***
+        for el in tok:
+        	score = score + math.log(c[1].get(el,1))
+        	
+        answers.append((score,c[0]))
+        f.close()
+    answers.sort()
+    answers.reverse()
+    return answers
+
+def files2countdict (files):
+    """Given an array of filenames, return a dictionary with keys
+    being the space-separated, lower-cased words, and the values being
+    the number of times that word occurred in the files."""
+    d = defaultdict(int)
+    for file in files:
+        for word in open(file).read().split():
+            d[word.lower()] += 1
+    return d
+
 		
 if __name__ == '__main__':
 	print "time to train"
